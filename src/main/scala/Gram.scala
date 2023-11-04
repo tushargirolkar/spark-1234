a2efb4a4c977aec5eb57fb7f8bf68f702c2d432c
Tushar <trgirolkar@gmail.com>
2023-11-04 13:10:12 +0530

    kg to gram convertion problem

--- src/main/scala/Gram.scala
+++ src/main/scala/Gram.scala
@@ -3,7 +3,7 @@ object Gram {
     def main(args: Array[String]): Unit = {
       var a = 56
       var grams = a * 1000
-      println("The convertion of 56kg into gram is " + grams +"Gm")
+      println("The convertion of 56 kg into gram is " + grams +"Gm")
     }
   }
 
